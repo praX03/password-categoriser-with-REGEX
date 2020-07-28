991ab0554d19d7ec8911d949044d650a58eae192
Pratham <68374911+praX03@users.noreply.github.com>
2020-07-28 14:24:47 +0530

    Add files via upload

---
+++ pass_cat.py
@@ -0,0 +1,27 @@
+import re
+
+# Define function
+def password(x):
+
+# Check it's not a space or blank line
+    if x == "\n" or x == " ":
+        return "Password cannot be empty"
+
+    # Check the password length
+    if 7 <= len(x) <= 20:
+        # Check for reptitive charracter occurence
+        if re.search(r'(.)\1\1', x):
+            return "Weak Password! Password has repeating characters more than three times"
+        # Check for repetitive string occurence
+        if re.search(r'(..)(.*?)\1', x):
+            return "Weak Password! Password has repeating strings more than once"
+        else:
+            return "Strong Password!"
+    else:
+        return "Password length should be between 7 - 20 characters."
+# Main method
+def main():
+    print(password(input()))
+# Driver Code
+if __name__ == "__main__":
+    main()
